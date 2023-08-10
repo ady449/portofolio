e65fa6904586f12521b2b3cf2f0559990c80c99c
adig24 <adrian.giurgiu@endava.com>
2023-08-10 11:58:23 +0300

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,41 @@
 import React from "react";
 
+import Carousel from "./carousel/carousel";
+
 function Projects() {
+  const projects = [
+    {
+      title: "Coming",
+      desc: null,
+      comp: null,
+      tags: [
+       
+      ],
+    },
+    {
+      title: "Soon!",
+      desc: null,
+      comp: null,
+      tags: [
+       
+      ],
+    },
+    
+  ];
   return (
     <div
       name="projects"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-screen  text-gray-300 bg-[#0a192f] flex justify-center items-center "
     >
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-          Projects
-        </p>
-        <p className="text-gray-300 py-4">Projects</p>
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-4">
+            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+              Projects
+            </p>
+          </div>
+        </div>
+        <Carousel projects={projects}></Carousel>
       </div>
     </div>
   );
