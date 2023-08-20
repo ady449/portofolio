7226681f31ba8d044fc7cc6f556ed5d71f212f4f
ady449 <adrian.giurgiu01@e-uvt.ro>
2023-08-20 10:26:50 +0300

    20/81 change

--- src/components/About.js
+++ src/components/About.js
@@ -1,95 +1,120 @@
 import React from "react";
 import Card from "../components/3dCard/Card.js";
-import AOS from 'aos';
+import AOS from "aos";
+import { FaReact, FaJava } from "react-icons/fa";
+import { DiCss3 } from "react-icons/di";
+import { FiDatabase } from "react-icons/fi";
+import { BiLogoSpringBoot, BiLogoAws } from "react-icons/bi";
+
 const About = () => {
-  AOS.init()
-  const projects = [
-    {
-      title: "Junior QA Software",
-      desc: "QA in insurance project",
-      comp:"Endava",
-      tags: [
-        {
-          name: "AWS",
-          color: "red-600",
-        },
-        {
-          name: "Java",
-          color: "green-600",
-        },
-        {
-          name: "Manual & Automation Testing",
-          color: "green-600",
-        },
-      ],
-    },
-    {
-      title: "QA Software",
-      desc: "Promoted to a mid position since 1 January 2023. He changed the teams repeatedly, completing the objectives.",
-      comp:"Endava",
-      tags: [
-        {
-          name: "AWS",
-          color: "red-600",
-        },
-        {
-          name: "Java",
-          color: "blue-600",
-        },
-        {
-          name: "Database",
-          color: "green-200",
-        },
-      ],
-    },
-  ];
-  return (
-    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-          <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-              About
-            </p>
-          </div>
-        </div>
-        <div className="max-w-[800px] pb-8 pl-4">
-          <p>
-            He graduated with a bachelor's degree in computer science from the
-            West University of Timisoara. Is a passionate about building
-            software that improves the lives of those around he.
-          </p>
-        </div>
-        <div className="max-w-[800px] w-full grid sm:grid-cols-2 gap-8 px-4">
-          <div className="flex-basis text-4xl font-bold">
-            <p className="text-2xl font-light text-center sm:pt-10 ">Work experience</p>
-          </div>
-          {projects.map((project, ind) => {
-            return (
-              <div
-                key={ind}
-                data-aos={"fade-up"}
-                data-aos-offset="100"
-                data-aos-delay={ind % 2 === 0 ? "0" : "700"}
-                data-aos-easing="ease-in-out"
-                data-aos-mirror="true"
-                data-aos-once="false"
-                data-aos-anchor-placement="top-center"
-                data-aos-duration="2000"
-              >
-                <Card
-                  title={project.title}
-                  comp={project.comp}
-                  desc={project.desc}
-                  tags={project.tags}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	AOS.init();
+	const projects = [
+		{
+			title: "Junior QA Software",
+			desc: "QA in insurance project",
+			comp: "Endava",
+			tags: [
+				{
+					name: "AWS",
+					color: "red-600",
+				},
+				{
+					name: "Java",
+					color: "green-600",
+				},
+				{
+					name: "Manual & Automation Testing",
+					color: "green-600",
+				},
+			],
+		},
+		{
+			title: "QA Software",
+			desc: "Promoted to a mid position since 1 January 2023. He changed the teams repeatedly, completing the objectives.",
+			comp: "Endava",
+			tags: [
+				{
+					name: "AWS",
+					color: "red-600",
+				},
+				{
+					name: "Java",
+					color: "blue-600",
+				},
+				{
+					name: "Database",
+					color: "green-200",
+				},
+			],
+		},
+	];
+	return (
+		<div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+			<div className="flex flex-col justify-center items-center w-full h-full">
+				<div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+					<div className="sm:text-right pb-8 pl-4">
+						<p className="text-4xl font-bold inline border-b-4 border-pink-600">
+							About
+						</p>
+					</div>
+				</div>
+				<div className="max-w-[800px] pb-8 pl-4">
+					<p>
+						He graduated with a bachelor's degree in computer science from the
+						West University of Timisoara. Is a passionate about building
+						software that improves the lives of those around he.
+					</p>
+				</div>
+				<div className="max-w-[800px] w-full grid sm:grid-cols-2 gap-8 px-4">
+					<div className="flex-basis text-4xl font-bold">
+						<p className="text-2xl font-light text-center sm:pt-10 ">
+							Work experience
+						</p>
+					</div>
+					{projects.map((project, ind) => {
+						return (
+							<div
+								key={ind}
+								data-aos={"fade-up"}
+								data-aos-offset="100"
+								data-aos-delay={ind % 2 === 0 ? "0" : "700"}
+								data-aos-easing="ease-in-out"
+								data-aos-mirror="true"
+								data-aos-once="false"
+								data-aos-anchor-placement="top-center"
+								data-aos-duration="2000"
+							>
+								<Card
+									title={project.title}
+									comp={project.comp}
+									desc={project.desc}
+									tags={project.tags}
+								/>
+							</div>
+						);
+					})}
+				</div>
+				{/* end sect card */}
+
+				<div className="tech mt-48 ">
+					<div className="flex flex-col md:flex-row p-10">
+						<p className="text-2xl text-center font-light max-md:pb-5 md:border-r-2 md:pr-5 max-md:border-b-2">
+							Tech
+						</p>
+						{/* <hr className="solid self-center w-14 md:hidden" /> */}
+						<div className="flex flex-row max-md:mt-5 md:ml-10 text-4xl space-x-5 ">
+							<FaReact className="hover:text-5xl" />
+							<FaJava className="hover:text-5xl" />
+							<DiCss3 className="hover:text-5xl" />
+							<BiLogoSpringBoot className="hover:text-5xl" />
+							<BiLogoAws className="hover:text-5xl" />
+							<FiDatabase className="hover:text-5xl" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,33 +2,33 @@ import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
-  return (
-    <div name="home" className="w-full h-screen bg-[#0a192f]">
-      {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p className="text-pink-600">He name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-          Giurgiu Adrian-Valentin
-        </h1>
-        <h2 className="text-3xl sm:text-4xl font-bold text-[#8892b0]">
-          software developer
-        </h2>
-        <p className="text-[#8892b0] py-4 max-w-[700px]">
-          He is higly motivated, organized and dedicated individuals who is
-          always willing to learn, share knowledge and ideas.He is a team
-          player, open to providing support on the development and testing side.
-        </p>
-        <div>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            View Work
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3 " />
-            </span>
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div name="home" className="w-full h-screen bg-[#0a192f]">
+			{/* Container */}
+			<div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+				<p className="text-pink-600">He name is</p>
+				<h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+					Giurgiu Adrian-Valentin
+				</h1>
+				<h2 className="text-3xl sm:text-4xl font-bold text-[#8892b0]">
+					software developer
+				</h2>
+				<p className="text-[#8892b0] py-4 max-w-[700px]">
+					He is higly motivated, organized and dedicated individuals who is
+					always willing to learn, share knowledge and ideas.He is a team
+					player, open to providing support on the development and testing side.
+				</p>
+				<div>
+					<button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+						View Work
+						<span className="group-hover:rotate-90 duration-300">
+							<HiArrowNarrowRight className="ml-3 " />
+						</span>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
