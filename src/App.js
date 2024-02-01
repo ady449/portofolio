086a5d1477b84d1c85994c878c12523db20d1457
ady449 <adrian.giurgiu01@e-uvt.ro>
2024-02-01 16:26:11 +0200

    1feb

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import Navbar from "./components/Navbar.js";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Projects from "./components/Projects";
-import usePreventZoom from "./components/preventZoom.js"
+import Layout from "./Layout";
+import usePreventZoom from "./components/preventZoom.js";
 function App() {
-  usePreventZoom();
-  return (
-    <div >
-      <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-    </div>
-  );
+	usePreventZoom();
+	return (
+		<Layout>
+			<Home />
+			<About />
+			<Projects />
+			<Contact />
+		</Layout>
+	);
 }
 
 export default App;
